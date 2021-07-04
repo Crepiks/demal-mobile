881b7218bbd8b5f3d8fb87f40dd49c3227356339
Crepiks <sayazhan.onlassyn@mail.ru>
2021-07-04 15:57:20 +0600

    feat: implement sliders on home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,61 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(child: TourCard());
+    return SafeArea(
+        child: Container(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 24),
+                      child: Text(
+                        "Популярные туры",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      ),
+                    )),
+                SizedBox(
+                  width: double.infinity,
+                  height: 260,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: EdgeInsets.only(
+                              right: 16, left: index == 0 ? 24 : 0),
+                          child: TourCard(),
+                        );
+                      }),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(top: 30, bottom: 16),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 24),
+                      child: Text(
+                        "Рекомендации для вас",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      ),
+                    )),
+                SizedBox(
+                  width: double.infinity,
+                  height: 260,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: EdgeInsets.only(
+                              right: 16, left: index == 0 ? 24 : 0),
+                          child: TourCard(),
+                        );
+                      }),
+                )
+              ],
+            )));
   }
 }
