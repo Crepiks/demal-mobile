521692b558e1bf96d2d7f2ff33442a44876443d5
Crepiks <sayazhan.onlassyn@mail.ru>
2021-07-10 16:34:26 +0600

    feat: implement detailed componenets of profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -34,16 +34,90 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         fontWeight: FontWeight.w600,
                         color: Colors.white),
                   ),
-                )
+                ),
+                Container(
+                    margin: EdgeInsets.only(top: 30, bottom: 10),
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: AssetImage("images/avatar.jpg"),
+                            fit: BoxFit.cover))),
+                Text("Саяжан Онласын",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white)),
+                Text("Алматы, Казахстан", style: TextStyle(color: Colors.white))
               ],
             ),
             Container(
               height: 400,
+              padding: EdgeInsets.only(top: 30),
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(30),
                       topRight: Radius.circular(30))),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.edit,
+                              size: 28,
+                              color: Colors.purple,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 16),
+                              child: Text(
+                                "Редактировать",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w500),
+                              ),
+                            )
+                          ],
+                        ),
+                        Icon(Icons.arrow_right_outlined,
+                            size: 30, color: Colors.purple)
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.credit_card,
+                              size: 28,
+                              color: Colors.purple,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 16),
+                              child: Text(
+                                "Способы оплаты",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w500),
+                              ),
+                            )
+                          ],
+                        ),
+                        Icon(Icons.arrow_right_outlined,
+                            size: 30, color: Colors.purple)
+                      ],
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         )));
